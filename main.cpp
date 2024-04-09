9a5c824b1bc1da1e053e3d4e03787c59e061ce80
Mazin <mazinhussain03@gmail.com>
2024-04-09 14:15:17 +0400

    Added Case 2 (error)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -32,36 +33,84 @@ class User{
             cout << "Mobile Number: +" << mobile_num << endl;
             cout << "Current Balance in Bank Account: " << balance_current << endl;
         }
+
+        bool operator==(const User& u){
+            if((f_name == u.f_name) && (l_name == u.l_name)){
+                return true;
+            }
+
+            return false;
+        }
+
+        User& operator=(const User& m){
+            if((f_name == m.f_name) && (l_name == m.l_name)){
+                age = m.age;
+                mobile_num = m.mobile_num;
+                balance_current = m.balance_current;
+                return *this;
+            }
+
+            f_name = m.f_name;
+            l_name = m.l_name;
+            age = m.age;
+            mobile_num = m.mobile_num;
+            balance_current = m.balance_current;
+
+            return *this;
+        }
 };
 
 int main(){
     int Login_choice;
-
-    cout << "WELCOME TO THE BANK PORTAL!!" << endl;
-    cout << "1)LogIn \n2)SignUp" << endl;
-
-    cout << "1 or 2: ";
-    cin >> Login_choice;
-
-    switch(Login_choice)
+    vector<User> User_List;
+    int n = 0;
+    string first_name, last_name;
+    while(n < 2)
     {
-        case 1:
-            string first_name;
-            string last_name;
-            cout << "\nFirst and Last name: ";
-            cin >> first_name >> last_name;
+        cout << "WELCOME TO THE BANK PORTAL!!" << endl;
+        cout << "\n1)SignUp \n2)LogIn" << endl;
 
-            int Age;
-            cout << "\nEnter your age: ";
-            cin >> Age;
+        cout << "\n1 or 2: ";
+        cin >> Login_choice;
 
-            int phone_num;
-            cout << "\nEnter Phone Number: ";
-            cin >> phone_num;
+        switch(Login_choice)
+        {
+            case 1:
+            {   
+                cout << "\nFirst and Last name: ";
+                cin >> first_name >> last_name;
 
-            User o(first_name, last_name, Age, phone_num, 3000);
-            cout << "\nDepositing intial 3000AED into account.......Successful" << endl;
-            break;
+                int Age;
+                cout << "\nEnter your age: ";
+                cin >> Age;
+
+                int phone_num;
+                cout << "\nEnter Phone Number: ";
+                cin >> phone_num;
+
+                User o(first_name, last_name, Age, phone_num, 3000);
+                cout << "\nDepositing intial 3000AED into account.......Successful" << endl;
+                break;
+            }
+            case 2:
+
+                cout << "\nFirst and Last Name: ";
+                cin >> first_name >> last_name;
+
+                User u(first_name,last_name,0,0,0);
+
+                for(int i = 0; i < User_List.size(); i++){
+                    if(u == User_List[i]){
+                        u = User_List[i];
+                    }
+                }
+                cout << endl;
+                u.printInfo();
+
+                break;
+        }
+
+        n+=1;
     }
 
     return 0;
